1f37770b921a967e5878399619b3917d9558b8b8
Vincent <vleclerc@inow.fr>
2020-10-09 14:25:09 +0200

    add attributes db

--- csharpdev/Venezia/Models/Car.cs
+++ csharpdev/Venezia/Models/Car.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Venezia.Models
 {
+    [Table("voitures", Schema = "venezia")]
     public class Car
     {
         public int ID { get; set; }
 
+        [Column("Marque")]
         [Display(Name ="Marque", Prompt = "Marque du véhicule")]
         public string Mark { get; set; }
 
@@ -19,6 +22,7 @@ namespace Venezia.Models
 
         [Display(Name = "Tarif", Prompt = "Tarif du véhicule")]
         [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal? Price { get; set; }
 
         [Display(Name = "Carburant", Prompt = "Carburant du véhicule")]
