c35a6640e5d7c323c4b4d245c3a3af86ff1f31d6
Vincent <vleclerc@inow.fr>
2020-10-09 12:55:30 +0200

    entity & attributes

--- csharpdev/Venezia/Models/Car.cs
+++ csharpdev/Venezia/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,22 @@ namespace Venezia.Models
     public class Car
     {
         public int ID { get; set; }
+
+        [Display(Name ="Marque", Prompt = "Marque du véhicule")]
         public string Mark { get; set; }
 
+        [Display(Name = "Modèle", Prompt = "Modèle du véhicule")]
+        [Required(ErrorMessage = "{0} obligatoire")]
         public string Model { get; set; }
 
-        public decimal Price { get; set; }
+        [Display(Name = "Tarif", Prompt = "Tarif du véhicule")]
+        [DataType(DataType.Currency)]
+        public decimal? Price { get; set; }
 
+        [Display(Name = "Carburant", Prompt = "Carburant du véhicule")]
         public Fuel FuelType { get; set; }
 
+        [Display(Name = "Voiture autonome")]
         public bool Autonomous { get; set; }
     }
 
