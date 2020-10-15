1a31cc03481ad5b225c2e768e9411342f3c5cbf4
Vincent <vleclerc@inow.fr>
2020-10-15 16:17:37 +0200

    add serilog

--- csharpdev/Venezia/Controllers/HomeController.cs
+++ csharpdev/Venezia/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Venezia.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Index de HomeController");
             return View();
         }
 
@@ -17,6 +26,7 @@ namespace Venezia.Controllers
         [Route("a-propos-de-nous", Name = "RouteAbout")]
         public IActionResult About()
         {
+            _logger.LogInformation("Index de HomeController");
             return View();
         }
     }
