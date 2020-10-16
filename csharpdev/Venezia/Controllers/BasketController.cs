a944312f10ff193a3c30b76f527f354cd7abfd9f
Vincent <vleclerc@inow.fr>
2020-10-16 10:03:32 +0200

    inti ajax

---
+++ csharpdev/Venezia/Controllers/BasketController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Venezia.Controllers
+{
+    public class BasketController : Controller
+    {
+        [HttpPost]
+        public JsonResult AddCarToBasket(int idCar)
+        {
+            
+            return null;
+        }
+    }
+}
