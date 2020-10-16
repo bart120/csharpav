bf035eb1130f777558060588ca84fb46de8959fe
Vincent <vleclerc@inow.fr>
2020-10-16 16:14:47 +0200

    display basket

--- csharpdev/Venezia/Controllers/BasketController.cs
+++ csharpdev/Venezia/Controllers/BasketController.cs
@@ -4,17 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Venezia.Data;
 using Venezia.Models;
 
 namespace Venezia.Controllers
 {
     public class BasketController : Controller
     {
+        private readonly VeneziaContext _context;
+        public BasketController(VeneziaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("mon-panier", Name = "BasketUrl")]
+        public async Task<IActionResult> Index()
+        {
+            var vm = new BasketViewModel();
+            if (this.HttpContext.Session.GetString("BASKET") != null)
+            {
+                var basket = JsonConvert.DeserializeObject<Basket>(this.HttpContext.Session.GetString("BASKET"));
+                foreach (var item in basket.Cars)
+                {
+                    var car = await _context.Car.Include(x => x.FuelType).SingleOrDefaultAsync(x => x.ID == item.Key);
+                    vm.Cars.Add(car, item.Value);
+                    vm.Total += car.Price.GetValueOrDefault() * item.Value;
+                }
+            }
+            return View(vm);
+        }
+
         [HttpPost]
         public IActionResult AddCarToBasket(int idCar)
         {
-           
+
             Basket basket;
             if (this.HttpContext.Session.GetString("BASKET") == null)
                 basket = new Basket();
@@ -25,7 +51,7 @@ namespace Venezia.Controllers
             }
             basket.AddCar(idCar);
             var json = basket.ToJson();
-            this.HttpContext.Session.SetString("BASKET",json);
+            this.HttpContext.Session.SetString("BASKET", json);
             return Ok(json);
         }
     }
